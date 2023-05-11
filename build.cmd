b2c0f3fa59ded5258c31dca52695247bb01e9885
Alex Fedorov <nobodygm@gmail.com>
2023-05-11 23:56:01 +0300

    Release files generation

--- build.cmd
+++ build.cmd
@@ -33,8 +33,8 @@ if not %x86status% == 0 (
   echo.
 )
 
-if not %x64status% == 0 goto cleanup
-if not %x86status% == 0 goto cleanup
+if not %x64status% == 0 goto exit
+if not %x86status% == 0 goto exit
 
 if exist bin\ (
 
